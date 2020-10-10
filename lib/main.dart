6e5e97a794486cf84f29bd36a670c51bebc83039
nvshah <nipunshah6776@gmail.com>
2020-10-10 13:46:51 +0530

    CRUD - Update, Delete accounted Minor bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,45 @@ void main() async {
   final appDocumentDir = await getApplicationDocumentsDirectory();
   // Door for Hive
   Hive.init(appDocumentDir.path);
+  //Registering Series Class for Hive Box-storage facilitation
   //starting from 0 & then 1,2,... goes on for other type id
   //? What is typeId ? -> typeId unique identifier to identify type
   Hive.registerAdapter(SeriesAdapter(), 0);
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hive Tutorial',
-      home: SeriesPage(),
+      title: 'Hive Concepts',
+      home: FutureBuilder(
+        //Open Hive Box to store data further
+        future: Hive.openBox('contacts'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError)
+              return Text(snapshot.error.toString());
+            else
+              return SeriesPage();
+          }
+          // Although opening a Box takes a very short time,
+          // we still need to return something before the Future completes.
+          else
+            return Scaffold();
+        },
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
   }
 }
--- lib/pages/series_page.dart
+++ lib/pages/series_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import './new_series_form.dart';
 import '../models/series.dart';
@@ -25,22 +26,42 @@ class SeriesPage extends StatelessWidget {
         ));
   }
 
-  ListView _buildListView() {
-    //first we need to get the series-box, and as we have defined the box at top of the 1st level,
-    //We will be able to open that box over here
-    final seriesBox = Hive.box('series');
-    
-    return ListView.builder(
-      itemCount: seriesBox.length,
-      itemBuilder: (BuildContext context, int idx){
-        //final series = seriesBox.get(idx) as Series;   // problematic when using put() or something deleted from Box
-        final series = seriesBox.getAt(idx) as Series;
+  //Build List of Series
+  Widget _buildListView() {
+    return WatchBoxBuilder(
+      //first we need to get the series-box, and as we have defined the box at top of the 1st level,
+      //We will be able to open that box over here
+      box: Hive.box('series'),
+      builder: (ctxt, seriesBox) {
+        return ListView.builder(
+          itemCount: seriesBox.length,
+          itemBuilder: (BuildContext context, int idx) {
+            //final series = seriesBox.get(idx) as Series;   // problematic when using put() or something deleted from Box
+            final series = seriesBox.getAt(idx) as Series;
 
-        return ListTile(
-          title: Text(series.name),
-          subtitle: Text(series.ratings.toString()),
+            return ListTile(
+              title: Text(series.name),
+              subtitle: Text(series.ratings.toString()),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  //UPDATE/Refresh
+                  IconButton(
+                    icon: Icon(Icons.refresh, color: Colors.indigo,),
+                    onPressed: (){
+                      return seriesBox.putAt(idx, Series('${series.name}', series.ratings + 1));
+                    },
+                  ),
+                  //DELETE
+                  IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red,),
+                    onPressed: () => seriesBox.deleteAt(idx),
+                  ),
+                ],
+              ),
+            );
+          },
         );
-
       },
     );
   }
